23584f72dc16d6159d0f8b797362336ee9f5c29a
Andrew Haeger <ajhaeger@svsu.edu>
2013-11-13 22:45:22 -0500

    Deleted Java Forms.  Rebuilt initial form.  Will re-build rest.

--- POST System/src/POSTSystem/POSTSystem.java
+++ POST System/src/POSTSystem/POSTSystem.java
@@ -14,6 +14,6 @@ public class POSTSystem {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    InitialDisplay temp = new InitialDisplay();
   }
 }
